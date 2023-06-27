fe03f4744cfb2c6b52b1b4c78f85eb0ea065cd82
jocile <jocile@gmail.com>
2023-06-27 20:35:14 -0300

    Criando o executável jar

--- src/main/java/com/jocile/cadastrodefuncionarios/Cadastrodefuncionarios.java
+++ src/main/java/com/jocile/cadastrodefuncionarios/Cadastrodefuncionarios.java
@@ -4,6 +4,8 @@
 
 package com.jocile.cadastrodefuncionarios;
 
+import com.jocile.cadastrodefuncionarios.view.principal;
+
 /**
  *
  * @author aluno
@@ -11,6 +13,6 @@ package com.jocile.cadastrodefuncionarios;
 public class Cadastrodefuncionarios {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new principal().setVisible(true);
     }
 }
